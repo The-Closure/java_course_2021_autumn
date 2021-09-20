00eb10be770c23ddcb3ffc519a8729876e059685
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-09-20 07:57:01 +0300

    init point

--- src/main/java/org/closure/App.java
+++ src/main/java/org/closure/App.java
@@ -6,8 +6,11 @@ package org.closure;
  */
 public class App {
     public static void main(String[] args) {
+
       User admin = new User("zain", 20, "ADMIN");
       admin.printInfo();
+      String birthView = String.format("%d", admin.calcBirthYear());
       System.out.println(String.format("birth year : %d", admin.calcBirthYear()));
+    
     }
 }
