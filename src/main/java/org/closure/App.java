12bbb18f21315f78f2bb954e44efa6e3df448e31
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-09-24 17:42:26 +0300

    init point

--- src/main/java/org/closure/App.java
+++ src/main/java/org/closure/App.java
@@ -5,39 +5,31 @@ package org.closure;
  *
  */
 public class App {
-    public static void main(String[] args) {
-      String url = "http://www.google.com";
-      String email = "thee.closure@gmail.com";
-      System.out.println(url.length());
-      
-      String[] results = url.split("\\.");
-      System.out.println(results[0]);
 
-      System.out.println(url.substring(5,10));
-
-      System.out.println(url.indexOf("www"));
-
-      System.out.println(url.charAt(7));
-
-      System.out.println(url.compareTo("http://www.google.coM"));
-
-      System.out.println(url.compareToIgnoreCase("Http://WwW.GooGlE.CoM"));
-
-      System.out.println(url.concat("?user=java&password=123"));
-
-      System.out.println(url.contains("www"));
-
-      System.out.println(url.contentEquals("http://www.google.coM"));
-
-      System.out.println(url.equalsIgnoreCase("http://www.google.coM"));
-
-      System.out.println(url.endsWith("net"));
-
-      System.out.println(url.startsWith("http"));
-      
-      String.format("url.com?var1=%d&var2=%s", 1,email);
-
-      String.valueOf(3);
-      
+  public static void checkAge(int i) throws Exception {
+    if (i > 18 && i < 42) {
+      throw new Exception("our army is waiting you");
     }
+    System.out.println("safety age");
+  }
+
+  public static void main(String[] args) {
+    String s = "test";
+    try {
+      System.out.println("connecting ...");
+      System.out.println(s.charAt(100)); // processing
+    } catch (Exception e) {
+      System.out.println("Exception occured");
+    } finally {
+      System.out.println("close");
+    }
+
+    try {
+      checkAge(20);
+      System.out.println("hi");
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+
+  }
 }
