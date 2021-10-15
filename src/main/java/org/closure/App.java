dce586e5c0e96a8c3049062ea0a10b12efed74e2
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-10-15 17:05:00 +0300

    jdbc init point

--- src/main/java/org/closure/App.java
+++ src/main/java/org/closure/App.java
@@ -1,58 +1,20 @@
 package org.closure;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-
+import java.sql.*;
 public class App {
-
+    public static final String DB_URL = "jdbc:mysql://localhost/springbootcourse";
+    public static final String USER = "root";
+    public static final String PASSWORD = "";
     public static void main(String[] args) {
-
-        ArrayList<String> kaaabour = new ArrayList<>();
-        kaaabour.add(0, "zero element");
-        kaaabour.clear();
-        kaaabour.add("6 element");
-        kaaabour.add("1 element");
-        kaaabour.add("4 element");
-        kaaabour.add("3 element");
-        kaaabour.add("1 element");
-        kaaabour.add("6 element");
-        kaaabour.set(0, "2 element");
-        Consumer<String> printConsumer = t -> {
-            System.out.println(t);
-        };
-        kaaabour.forEach(t -> System.out.println(t));
-        // Predicate
-        
-        kaaabour.stream().map(t -> t.charAt(0)).toList().forEach(t-> System.out.println(t));
-        kaaabour.stream().filter(t-> {
-            char c = t.charAt(0);
-            int a = c;
-            a = a - 48;
-            return a >= 5;
-        }).forEach(t-> System.out.println("filtered items : "+t));
-        
-        kaaabour.stream().sorted((u,v)->{
-            char c = u.charAt(0);
-            int a = c;
-            a = a - 48;
-
-            char d = v.charAt(0);
-            int b = d;
-            b = b - 48;
-
-            return a - b;
-        }).forEach(t->System.out.println("sorted items : " + t));
-        kaaabour.removeIf(e -> true);
-        HashMap<String, User> map = new HashMap<String, User>();
-        map.put("admin", new User(1, "name"));
-        map.put("emp", new User(2, "employee"));
-        map.put("guest", new User(3, "Guest"));
-        
-        
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            Statement stat = conn.createStatement();
+            ResultSet rs = stat.executeQuery("SELECT * FROM user WHERE id=4");
+            rs.next();
+            System.out.println("your password has been hacked : "+rs.getString("user_name"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
 }
