15a5ecc53e4d1de80b434cf7983d9f21aa183b70
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-10-16 14:43:29 +0300

    init point

--- src/main/java/org/closure/App.java
+++ src/main/java/org/closure/App.java
@@ -1,21 +1,26 @@
 package org.closure;
 
 import java.sql.*;
+
 public class App {
     public static final String DB_URL = "jdbc:mysql://localhost/springbootcourse";
     public static final String USER = "root";
     public static final String PASSWORD = "";
+
     public static void main(String[] args) {
         int rows = 0;
         boolean state = false;
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-            conn.setAutoCommit(true);
-            PreparedStatement stat = conn.prepareStatement("INSERT INTO user (is_active,password,roles,user_name) VALUES (?,'p@SSW0RD',?,'CPP')");
-            stat.setBoolean(1, true);
-            stat.setString(2, "ADMIN_ROLE");   
-            stat.executeUpdate();
-        
+            Statement stat = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            ResultSet rs = stat.executeQuery("SELECT * FROM user");
+            while (rs.next()) {
+                System.out.println(rs.getString("password"));
+                while (rs.previous()) {
+                    System.out.println(rs.getString("password"));
+                }
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
