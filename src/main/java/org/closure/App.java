6cd55bb85a6b84e4970cd45b031677f467703286
anas_altarazi <anas.anas1998,tar@gmail.com>
2021-10-16 14:26:26 +0300

    init point

--- src/main/java/org/closure/App.java
+++ src/main/java/org/closure/App.java
@@ -10,14 +10,12 @@ public class App {
         boolean state = false;
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-            Statement stat = conn.createStatement();
-            ResultSet rs = stat.executeQuery("SELECT * FROM user WHERE id=4");
-            rs.next();
-            System.out.println("your password has been hacked : "+rs.getString("user_name"));
-            rows = stat.executeUpdate("INSERT INTO user (id,user_name,password,is_active) VALUES ('java','P@ssw0rd',1)");
-            rows = stat.executeUpdate("UPDATE user set is_active = 0");
-            state = stat.execute("SELECT * FROM user WHERE id=4");
-            state = stat.execute("UPDATE user set is_active = 0");
+            conn.setAutoCommit(true);
+            PreparedStatement stat = conn.prepareStatement("INSERT INTO user (is_active,password,roles,user_name) VALUES (?,'p@SSW0RD',?,'CPP')");
+            stat.setBoolean(1, true);
+            stat.setString(2, "ADMIN_ROLE");   
+            stat.executeUpdate();
+        
         } catch (SQLException e) {
             e.printStackTrace();
         }
